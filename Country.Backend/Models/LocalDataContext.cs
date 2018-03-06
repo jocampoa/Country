7eeceef534b9269aad82d4a788b6e9f01108613c
Juan David Ocampo Arias <Jocampoa@intergrupo.com>
2018-03-06 11:39:49 -0500

    Revert "Revert "Users in Backend""
    This reverts commit 7f9fe172d7815350280b41eac1478af00b750802.

---
+++ Country.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Country.Backend.Models
+{
+    using Domain;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Country.Domain.User> Users { get; set; }
+    }
+}
--- Country/Country/MainPage.xaml.cs
+++ Country/Country/MainPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿namespace Country
 {
     using Xamarin.Forms;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     public partial class MainPage : ContentPage
 	{
 		public MainPage()
